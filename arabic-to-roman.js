caf565efc32fb3bd0d2284c3bc3dd5a9484b899c
rizal ghod <rijalghodi10@gmail.com>
2022-08-26 10:11:46 +0700

    edit the comment

--- arabic-to-roman.js
+++ arabic-to-roman.js
@@ -1,7 +1,15 @@
-// Define the member of roman numbers
-// Every element index in ROMAN_NUM array represent the digitPosition of decimal number
-// first means roman number apear in the first glance in digit position of decimal number
-// second menas second, last means last.
+/*
+Created by Rijal Ghodi on August 25, 2022
+email : rijalghodi10@gmail.com
+github : https://github.com/rijalghodi
+
+This is javascript code to convert arabic number into roman number
+*/
+
+// + Define the components of roman numbers
+// roman numbers is wraped in an array named ROMAN_NUM. Each element of array
+// contains an object representing roman numbers components in every
+// decimal digit position
 const ROMAN_NUM = [
   {
     first: "I",
@@ -26,35 +34,41 @@ const ROMAN_NUM = [
   },
 ];
 
-// Define function to convert decimal to roman
+// + Define function to convert decimal to roman
 function convertToRoman(num) {
   const digitsLength = num.toString().length;
 
-  // var roman will be the roman number version of decimal
+  // + Prepare the variable to store roman number
   let roman = "";
 
+  // for every digit in arabic number, do:
   for (let digitPosition = digitsLength; digitPosition > 0; digitPosition--) {
-    //extract digit from the decimal number
-    num = num % Math.pow(10, digitPosition);
-    let digitValue = Math.floor(num / Math.pow(10, digitPosition - 1));
+    // + Extract digits from the decimal number
+    let num = num % Math.pow(10, digitPosition);
+    let digit = Math.floor(num / Math.pow(10, digitPosition - 1));
 
-    // convert each digit to roman number
+    // + Convert each digit to roman number
     romanPosition = digitPosition - 1;
-    if (digitValue == 9) {
+    // if the digit == 9, add the the first and last roman number, such as IX
+    if (digit == 9) {
       roman = roman + ROMAN_NUM[romanPosition].first + ROMAN_NUM[romanPosition].last;
-    } else if (digitValue == 4) {
+    }
+    // if the digit == 4, add the first and middle roman number, such as IV
+    else if (digit == 4) {
       roman = roman + ROMAN_NUM[romanPosition].first + ROMAN_NUM[romanPosition].middle;
     } else {
-      if (digitValue >= 5) {
+      // if the digit >= 5, add the middle roman number (V, L, D, or #V)
+      // according to romanPosition
+      if (digit >= 5) {
         roman = roman + ROMAN_NUM[romanPosition].middle;
       }
-      // concat the
-      for (let i = 0; i < digitValue % 5; i += 1) {
+      // then add the fist roman number to complete that digit
+      for (let i = 0; i < digit % 5; i += 1) {
         roman = roman + ROMAN_NUM[romanPosition].first;
       }
     }
   }
   return roman;
 }
-
+// + test
 console.log(convertToRoman(36));
